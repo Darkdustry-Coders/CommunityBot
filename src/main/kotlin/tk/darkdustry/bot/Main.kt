86eb60242309e1d4d888cbb69e433d9fc80c97bb
lucin <theeugenet@gmail.com>
2022-09-15 20:39:08 +0700

    Недопонятый гений

--- src/main/kotlin/tk/darkdustry/bot/Main.kt
+++ src/main/kotlin/tk/darkdustry/bot/Main.kt
@@ -1,34 +1,47 @@
 package tk.darkdustry.bot
 
-import arc.util.Log
+import arc.files.Fi
+import arc.util.Log.info
+import arc.util.serialization.Json
 import arc.util.serialization.JsonWriter.OutputType
+import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
-import net.dv8tion.jda.api.requests.GatewayIntent.*
-import tk.darkdustry.bot.Vars.*
+import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel
+import net.dv8tion.jda.api.requests.GatewayIntent.GUILD_MEMBERS
+import net.dv8tion.jda.api.requests.GatewayIntent.MESSAGE_CONTENT
 
-class Main {
+lateinit var config: Config
 
-    fun main(args: Array<String>) {
-        json.setOutputType(OutputType.json)
-        json.setUsePrototypes(false)
+lateinit var jda: JDA
+lateinit var guild: Guild
 
-        val file = dataDirectory.child("config.json")
-        if (file.exists()) {
-            config = json.fromJson(Config::class.java, file.reader())
-            Log.info("Config loaded. (@)", file.absolutePath())
-        } else {
-            file.writeString(json.toJson(Config().also { config = it }))
-            Log.info("Config file generated. (@)", file.absolutePath())
-        }
+lateinit var mapsChannel: TextChannel
+lateinit var schematicsChannel: TextChannel
 
-        jda = JDABuilder.createLight(config.token)
-            .enableIntents(GUILD_MEMBERS, MESSAGE_CONTENT)
-            .build()
-            .awaitReady()
+val json = Json()
+val dataDirectory = Fi(".community")
 
-        guild = jda.getGuildById(config.guildId)!!
+fun main() {
+    json.setOutputType(OutputType.json)
+    json.setUsePrototypes(false)
 
-        mapsChannel = guild.getTextChannelById(config.mapsChannelId)!!
-        schematicsChannel = guild.getTextChannelById(config.schematicsChannelId)!!
+    val file = dataDirectory.child("config.json")
+    if (file.exists()) {
+        config = json.fromJson(Config::class.java, file.reader())
+        info("Config loaded. (@)", file.absolutePath())
+    } else {
+        file.writeString(json.toJson(Config().also { config = it }))
+        info("Config file generated. (@)", file.absolutePath())
     }
+
+    jda = JDABuilder.createLight(config.token)
+        .enableIntents(GUILD_MEMBERS, MESSAGE_CONTENT)
+        .build()
+        .awaitReady()
+
+    guild = jda.getGuildById(config.guildId)!!
+
+    mapsChannel = guild.getTextChannelById(config.mapsChannelId)!!
+    schematicsChannel = guild.getTextChannelById(config.schematicsChannelId)!!
 }
